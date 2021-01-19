ed47d7e26ce3fdd2f27ebe7d137a0226e52597e1
Jack A Crish Jr <jcrish@gmail.com>
2021-01-18 19:36:22 -0500

    better else
    We don't see the stack trace though.

--- styledLogs.js
+++ styledLogs.js
@@ -11,20 +11,17 @@ function styledOptions(message, data, options){
   //if an options object appears update the default object properties
   if(options){
     opts = {...options};
-
-    //if there's data add it to the package otherwise 'undefined' appears in the logs.
     if(data){
-      
-      return console.log(`%c${message}`, `background-color:${opts.background}; font-size:${opts.fontSize}; font-family:${opts.font}; color:${opts.color};`, data );
-
-    }else{    
+      return console.log(`%c${message}`, `background-color:${opts.background}; font-size:${opts.fontSize}; font-family:${opts.font}; color:${opts.color};`, data);
+    }else{
       return console.log(`%c${message}`, `background-color:${opts.background}; font-size:${opts.fontSize}; font-family:${opts.font}; color:${opts.color};`);
     }
-
   }else{
-  
-    return console.log(`%c${message}`, `background-color:${opts.background}; font-size:${opts.fontSize}; font-family:${opts.font}; color:${opts.color};`, ( data ? data : '' ) );
-  
+    if( data){
+      return console.log(`%c${message}`, `background-color:${opts.background}; font-size:${opts.fontSize}; font-family:${opts.font}; color:${opts.color};`, data );
+    }else{
+      return console.log(`%c${message}`, `background-color:${opts.background}; font-size:${opts.fontSize}; font-family:${opts.font}; color:${opts.color};`);
+    }
   }
 
 }
